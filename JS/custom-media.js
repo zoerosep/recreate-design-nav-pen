5f72123828554ceeaa533a2302300c9c40f12828
zoerosep-handle <zoerosepallas@gmail.com>
2026-09-11 11:14:45 -0500

    project

---
+++ JS/custom-media.js
@@ -0,0 +1,7 @@
+const buttonVariable2 = document.querySelector(".button2");
+
+function logButtonTwo() {
+  console.log("Button 2 was clicked!");
+}
+
+buttonVariable2.addEventListener("click", logButtonTwo);
